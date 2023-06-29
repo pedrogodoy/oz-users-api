970f6621da0c48a6d5df20e94f6a0f017521f774
Pedro <pedrogh.godoy12@gmail.com>
2023-06-29 16:36:11 -0300

    feat(password): create bcrypt utils for password

---
+++ src/utils/passwordUtils.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt');
+
+const saltRounds = 10;
+
+async function hashPassword(password) {
+  try {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    return hashedPassword;
+  } catch (error) {
+    throw new Error('Error hashing password');
+  }
+}
+
+async function comparePasswords(password, hashedPassword) {
+  try {
+    const match = await bcrypt.compare(password, hashedPassword);
+    return match;
+  } catch (error) {
+    throw new Error('Error comparing passwords');
+  }
+}
+
+module.exports = {
+  hashPassword,
+  comparePasswords,
+};
